5f74f82f4dae28c3e40f3e635794821f89270c3a
iqbal-elham <iqbal.elham7@gmail.com>
2023-06-13 23:04:29 +0430

    Add update_counter and recent_comments method to post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
     belongs_to :author, class_name: 'User'
     has_many :comments
     has_many :likes
+
+    def update_counters 
+        User.find(author_id).update(post_counter: Post.where(author_id).count)
+    end
+
+    def recent_comments
+        Comment.where(post_id: id).order(created_at: :desc).limit(5)
+    end
 end
