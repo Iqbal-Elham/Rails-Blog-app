6d6b17fb9c4e48aef88a55f7f67c0610f92e03c4
iqbal-elham <iqbal.elham7@gmail.com>
2023-06-17 16:14:02 +0430

    Write test for the user model attributes and methods

---
+++ spec/user_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before(:each) do
+    @user = User.create(name: 'John Doe', photo: 'https://i.pravatar.cc/300', bio: 'Alien biologist', post_counter: 0)
+  end
+
+  describe 'initialize' do
+    it 'should be valid' do
+      expect(@user.valid?).to be(true)
+    end
+
+    it 'should be a user' do
+      expect(@user).to be_a(User)
+    end
+
+    it 'should have a name' do
+      expect(@user.name).to eq('John Doe')
+    end
+
+    it 'should have a photo' do
+      expect(@user.photo).to eq('https://i.pravatar.cc/300')
+    end
+
+    it 'should have a bio' do
+      expect(@user.bio).to eq('Alien biologist')
+    end
+
+    it 'should have a posts counter' do
+      expect(@user.post_counter).to eq(0)
+    end
+  end
+
+  describe 'validations' do
+    it 'should validate presence of name' do
+      @user.name = nil
+      expect(@user.valid?).to be(false)
+      expect(@user.errors.include?(:name)).to be(true)
+    end
+
+    it 'should validate numericality of posts counter' do
+      @user.post_counter = 'a'
+      expect(@user.valid?).to be(false)
+      expect(@user.errors.include?(:post_counter)).to be(true)
+
+      @user.post_counter = -1
+      expect(@user.valid?).to be(false)
+      expect(@user.errors.include?(:post_counter)).to be(true)
+    end
+  end
+
+  describe 'methods' do
+    it 'should return 3 last posts' do
+      4.times do |i|
+        Post.create(author_id: @user.id, title: "Post ##{i}", text: "This is post ##{i}", comments_counter: 0, likes_counter: 0)
+      end
+      puts Post.first
+      expect(@user.recent_posts.count).to eq(3)
+    end
+  end
+end
