cb90d02eb94800266383a4943be2ec9bfe59aa67
iqbal-elham <iqbal.elham7@gmail.com>
2023-06-20 21:18:47 +0430

    Write test for the users controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,16 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  before(:each) do
+    get users_path
+  end
+
+  describe 'GET /users' do
+    it 'should return users' do
+      get '/users'
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /users/:id' do
+    it 'should return one user' do
+      get '/users/1'
+      expect(response).to have_http_status(200)
+    end
+  end
+
   describe 'GET /index' do
-    it 'returns http success' do
-      get '/users/index'
-      expect(response).to have_http_status(:success)
+    it 'renders a successful template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'renders the correct content' do
+      expect(response.body).to include('users')
     end
   end
 
   describe 'GET /show' do
+    before(:each) do
+      @user = User.create(name: 'Iqbal Elham', photo: 'https://www.images.com', bio: 'Hello, Iqbal Elham', post_counter: 0)
+      get users_path(@user)
+    end
+
     it 'returns http success' do
-      get '/users/show'
       expect(response).to have_http_status(:success)
     end
   end
