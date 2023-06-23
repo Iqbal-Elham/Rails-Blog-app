adbf5a4052cd127837f98b05aa8b267e13fcd157
iqbal-elham <iqbal.elham7@gmail.com>
2023-06-23 19:56:07 +0430

    Modify posts and users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
-  has_many :comments, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
+  has_many :comments
+  has_many :likes
 
   validates :name, presence: { message: 'Name can\'t be blank' }
   validates :post_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
